8aad108cb40934b73135acc49ab75345e38f312c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-22 15:21:29 +0000

    Deploy docs.quic.tech to github.com/cloudflare/quiche.git:gh-pages

---
+++ implementors/core/default/trait.Default.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["quiche"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"quiche/struct.RangeBuf.html\" title=\"struct quiche::RangeBuf\">RangeBuf</a>",synthetic:false,types:["quiche::stream::RangeBuf"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/core/ops/deref/trait.Deref.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["quiche"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"quiche/struct.RangeBuf.html\" title=\"struct quiche::RangeBuf\">RangeBuf</a>",synthetic:false,types:["quiche::stream::RangeBuf"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
